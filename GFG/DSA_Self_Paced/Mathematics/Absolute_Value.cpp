9771b14aebcddff925ff09d535e0ea0ad292c2a0
Hareram kumar <113000124+hareram1@users.noreply.github.com>
2024-07-14 16:21:39 +0530

    Absolute_Value.cpp

---
+++ GFG/DSA_Self_Paced/Mathematics/Absolute_Value.cpp
@@ -0,0 +1,56 @@
+/*
+
+// Example 1:
+
+    Input:
+    I = -32
+    Output: 32
+    Explanation: 
+    The absolute value of -32 is 32.
+ 
+
+// Example 2:
+
+    Input:
+    I = 45
+    Output: 45
+    Explanation: 
+    The absolute value of 45 is 45 itself.
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    public:
+        int absolute(int I) {
+        // Your code goes here
+        if(I<0){
+            I=I*(-1);
+        }
+        else{
+            I=I*1;
+        }
+        return I;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;    // number of testcases
+    cin >> T; // input number of testcases
+    while (T--) {
+        int I;
+        cin >> I;
+        Solution ob;
+        cout << ob.absolute(I) << endl; // print the output
+    }
+    return 0;
+}
+// } Driver Code Ends
