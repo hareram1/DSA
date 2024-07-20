f609e296797f17100e1e1d590998a219c4694bfd
Hareram kumar <113000124+hareram1@users.noreply.github.com>
2024-07-20 21:23:30 +0530

    Create Addition_Under_Modulo.cpp

---
+++ GFG/DSA_Self_Paced/Mathematics/Addition_Under_Modulo.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+        long long sumUnderModulo(long long a,long long b)
+    {
+        // code here
+         long long mod = 1e9 + 7;
+            // Compute the sum under modulo
+            return (a % mod + b % mod) % mod;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    long long a;
+	    long long b;
+	    cin>>a>>b;
+	    Solution ob;
+	    cout<<ob.sumUnderModulo(a,b)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
