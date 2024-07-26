28614006fbc76906e751a65b2dbfdd0eb957d4ed
Hareram kumar <113000124+hareram1@users.noreply.github.com>
2024-07-26 22:49:18 +0530

    Create Digital_Root.cpp

---
+++ GFG/DSA_Self_Paced/Recursion/Digital_Root.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    //Complete this function
+    int digitalRoot(int n) {
+        if (n == 0) return 0;
+        return 1 + (n - 1) % 9;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+	int T;
+	cin>>T;//taking testcases
+	while(T--)
+	{
+	    int n;
+	    cin>>n;//taking number n
+	    
+	    //calling digitalRoot() function
+	    Solution obj;
+	    cout<<obj.digitalRoot(n)<<endl;
+	    
+	    
+	}
+	return 0;
+}
+// } Driver Code Ends
