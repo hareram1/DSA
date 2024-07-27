b36748ea3391abeb1e8d4a758796d04e204b93f6
Hareram kumar <113000124+hareram1@users.noreply.github.com>
2024-07-27 23:55:32 +0530

    Create Possible Words_From_Phone_Digits.cpp

---
+++ GFG/DSA_Self_Paced/Recursion/Possible Words_From_Phone_Digits.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+    vector<string> possibleWords(int a[], int N)
+    {
+        vector<string> keypad = {
+            "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+        vector<string> result;
+        function<void(int, string)> backtrack = [&](int index, string current) {
+            if (index == N) {
+                result.push_back(current);
+                return;
+            }
+            string letters = keypad[a[index]];
+            for (char ch : letters) {
+                backtrack(index + 1, current + ch);
+            }
+        };
+        backtrack(0, "");
+        return result;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+  
+	int T;
+	
+	cin >> T; //testcases
+	
+	while(T--){ //while testcases exist
+	   int N;
+	    
+	   cin >> N; //input size of array
+	   
+	   int a[N]; //declare the array
+	   
+	   for(int i =0;i<N;i++){
+	       cin >> a[i]; //input the elements of array that are keys to be pressed
+	   }
+	   
+	   Solution obj;
+	   
+	  vector <string> res = obj.possibleWords(a,N);
+	  for (string i : res) cout << i << " ";
+	   cout << endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
