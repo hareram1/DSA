1f50bc45c4d5d3f96b77e6b4b19b2e77bf974d6a
Hareram kumar <113000124+hareram1@users.noreply.github.com>
2024-09-24 23:44:00 +0530

    Update Exactly_3_Divisors.cpp

--- GFG/DSA_Self_Paced/Mathematics/Exactly_3_Divisors.cpp
+++ GFG/DSA_Self_Paced/Mathematics/Exactly_3_Divisors.cpp
@@ -7,74 +7,31 @@ using namespace std;
 
 // } Driver Code Ends
 //User function Template for C++
-bool isPrime(int n)
-{
-    if(n==1)
-        return false;
-    if(n==2 || n==3)
-        return true;
-    if(n%2==0 || n%3==0)
-        return false;
-    for(int i=5;i*i<=n;i=i+6)
-    {
-        if(n%i==0 ||n%(i+2)==0)
-            return false;
-    }
-    return true;
-}
-
-int countPrimeFactors(int n)
-{
-    int count = 0;
-    while(n%2==0)
-    {
-        count++;
-        n=n/2;
-    }
-    while(n%3==0)
-    {
-        count++;
-        n=n/3;
-    }
-    for(int i=5;i*i<=n;i=i+6)
-    {
-        while(n%i==0)
-        {
-            count++;
-            n=n/i;
-        }
-        while(n%(i+2)==0)
-        {
-            count++;
-            n=n/(i+2);
-        }
-    }
-    if(n>3)
-        count++;
-    return count;
-}
 
 class Solution{
     public:
     int exactly3Divisors(int N)
     {
-        //Your code here
-        if(N==1)
-            return 0;
-        int num = 0;
-        if(N>4)
-            num++;
-        for(int i=3;i*i<=N;i+=2)
-        {
-            if(isPrime(i))
-            {
-                num++;
+        int count = 0;
+
+    for (int i = 2; i * i <= N; i++) {
+        bool isPrime = true;
+
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
             }
         }
-        return num;
+
+        if (isPrime)
+            count++;
+    }
+
+    return count;
+        //Your code here
     }
 };
-
 
 //{ Driver Code Starts.
 
